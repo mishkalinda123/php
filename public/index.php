23ba95c5c8a83b8a170517c92687e715a5319dd5
mishka <2567510@mail.ru>
2021-11-06 00:15:48 +0300

    new

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$A=23;
+$B=532;
+//echo $A*$B;
+
+
+$A ="I love";
+$B =" my wife!";
+//echo $A.$B;
+
+
+$A=5;
+$B=10;
+//echo ($A+$B)*($A+$A)*($A*$B)/100;
+
+$A=3;
+$B=x;
+$C=7;
+var_dump ($A+$B=$C);
+
+
+
+
+
